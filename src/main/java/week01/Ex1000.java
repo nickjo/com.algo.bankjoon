06e72c80c68c7c57156fc62a691907fb608ad7d1
origin <nickjo180@gmail.com>
2017-07-29 18:10:01 +0900

    Git Algo

---
+++ src/main/java/week01/Ex1000.java
@@ -0,0 +1,21 @@
+package week01;
+
+import java.util.Scanner;
+
+/**
+ * Created by nick on 17. 7. 29.
+ */
+public class Ex1000 {
+    public static void main(String[] args) {
+        int a,b;
+
+        Scanner sc = new Scanner(System.in);
+
+        a = Integer.parseInt(sc.next());
+        b = Integer.parseInt(sc.next());
+
+        System.out.println(a + b);
+
+
+    }
+}
---
+++ src/main/java/week01/Ex2557.java
@@ -0,0 +1,10 @@
+package week01;
+
+/**
+ * Created by nick on 17. 7. 29.
+ */
+public class Ex2557 {
+    public static void main(String[] args) {
+        System.out.printf("Hello World!");
+    }
+}
